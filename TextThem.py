69c9b352982043b09d3fb896686fba6d9c862c1c
DevMoore94 <brmo00206@gmail.com>
2014-10-03 12:38:27 -0300

    Added error checking and sending on click of send button.

--- TextThem.py
+++ TextThem.py
@@ -10,9 +10,17 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     
-	
-	requests.post(os.environ['BLOWERIO_URL'] + '/messages', data={'to': '+15062278951', 'message': 'Hello from Blower.io'})
-	return render_template('layout.html')
+    error = None
+    if request.method == 'POST':
+		
+		if(request.form['number'] == "" or request.form['message']):
+			error = "ERROR: Please fill in the above fields"
+		else:
+			requests.post(os.environ['BLOWERIO_URL'] + '/messages', data={'to': '+' + [request.form['number'], 'message': [request.form['message']})
+
+
+
+	return render_template('layout.html', error=error)
 
 
 if __name__ == '__main__':
