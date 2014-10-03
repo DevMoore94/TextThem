fe9769af062891dc5f570cb5373c1f5ed38f6c28
DevMoore94 <brmo00206@gmail.com>
2014-10-03 12:03:10 -0300

    Testing blower.io

--- TextThem.py
+++ TextThem.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from contextlib import closing
+import requests
 
 #create app
 app = Flask(__name__)
@@ -9,6 +10,10 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     
+	
+
+	requests.post(os.environ['BLOWERIO_URL'] + '/messages', data={'to': '+15062278951', 'message': 'Hello from Blower.io'})
+
     return render_template('layout.html')
 
 
