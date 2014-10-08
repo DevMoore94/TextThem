4662bf156352583f958047a5118e2aab7cba2d58
DevMoore94 <brmo00206@gmail.com>
2014-10-08 16:05:30 -0300

    Fixed DATABASE_URL

--- TextThem.py
+++ TextThem.py
@@ -13,12 +13,34 @@ import random
 
 
 
-
 #create app
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "os.environ['DATABASE_URL']"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
+
+class User(db.Model):
+  __tablename__ = 'users'
+  uid = db.Column(db.Integer, primary_key = True)
+  firstname = db.Column(db.String(100))
+  lastname = db.Column(db.String(100))
+  username = db.Column(db.String(100))
+  email = db.Column(db.String(120), unique=True)
+  pwdhash = db.Column(db.String(54))
+   
+  def __init__(self, firstname, lastname, email, password,username):
+    self.firstname = firstname.title()
+    self.lastname = lastname.title()
+    self.username = username
+    self.email = email.lower()
+    self.set_password(password)
+     
+  def set_password(self, password):
+    self.pwdhash = generate_password_hash(password)
+   
+  def check_password(self, password):
+    return check_password_hash(self.pwdhash, password)
 
 
 
