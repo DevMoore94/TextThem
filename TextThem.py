10ba7baa009ae6e9b7c715cef425bf7c73b62abe
DevMoore94 <brmo00206@gmail.com>
2014-10-09 14:18:36 -0300

    commented out flask-login stuff to try and fix prod.

--- TextThem.py
+++ TextThem.py
@@ -13,22 +13,22 @@ from flask.ext.openid import OpenID
 
 
 #path to tmp folder for openID
-open_basedir = "static/tmp"
+#open_basedir = "static/tmp"
 
 
 #create app
 app = Flask(__name__)
 
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://xqogpkihzswsuo:GHLg4AsJTF7rgyJv5fa3hj3dxI@ec2-184-73-194-196.compute-1.amazonaws.com:5432/d5a4164ud0gk36"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://xqogpkihzswsuo:GHLg4AsJTF7rgyJv5fa3hj3dxI@ec2-184-73-194-196.compute-1.amazonaws.com:5432/d5a4164ud0gk36"
 db = SQLAlchemy(app)
 
 
 #setups flask-login
-lm = LoginManager()
-lm.init_app(app)
-oid = OpenID(app,open_basedir)
+#lm = LoginManager()
+#lm.init_app(app)
+#oid = OpenID(app,open_basedir)
 
 class User(db.Model):
  
@@ -67,9 +67,9 @@ class User(db.Model):
    	return '<User %r>' % (self.username)    
      
 
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+#@lm.user_loader
+#def load_user(id):
+    #return User.query.get(int(id))
 
 
 
