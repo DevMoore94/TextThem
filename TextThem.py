6f02261b2b420268c9d66a16331f69ffbdce0f47
DevMoore94 <brmo00206@gmail.com>
2014-10-09 08:58:31 -0300

    Changed parameters

--- TextThem.py
+++ TextThem.py
@@ -29,7 +29,7 @@ class User(db.Model):
   email = db.Column(db.String(120), unique=True)
   password = db.Column(db.String(54))
    
-  def __init__(self, firstname, lastname, email, password,username):
+  def __init__(self, firstname, lastname, username, email, password):
     self.firstname = firstname.title()
     self.lastname = lastname.title()
     self.username = username
