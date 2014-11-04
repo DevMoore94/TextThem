4a373384125ec464d6c693dc45ebcfcc06b6c78e
DevMoore94 <brmo00206@gmail.com>
2014-11-04 10:47:59 -0400

    Added validation

--- static/phonebook_modal.js
+++ static/phonebook_modal.js
@@ -4,5 +4,6 @@ $(document).ready(function(){
           var str = ($("#contactForm input[type='radio']:checked").val());
           var str_parsed = str.split(" - ")
           $('#InputNumber').val(str_parsed[1])
+          $('#InputNumber').trigger('change');
         })
 });
