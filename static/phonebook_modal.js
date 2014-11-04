062a5d02d4ddf61969de097431e6529a3b2dec66
DevMoore94 <brmo00206@gmail.com>
2014-11-04 12:38:49 -0400

    Reverted changes.

--- static/phonebook_modal.js
+++ static/phonebook_modal.js
@@ -1,11 +1,3 @@
-function updateSubmitStatus(){
-	if($("#InputNumber").parent().hasClass("has-success") && $("#InputName").parent().hasClass("has-success")){
-		$("#addContactbtn").removeClass("disabled");
-	} else{
-		$("#addContactbtn").addClass("disabled");
-	}
-}
-
 $(document).ready(function(){
 
 	$('#phonebookModal').on('hidden.bs.modal', function () {
@@ -13,20 +5,7 @@ $(document).ready(function(){
           var str_parsed = str.split(" - ")
           $('#InputNumber').val(str_parsed[1])
           $('#InputNumber').trigger('change');
-
-          var current_number = $(this).val();
-
-          if(current_number.length > 0){
-			$(this).parent().removeClass("has-error");
-			$(this).parent().addClass("has-success");
-		} else {
-			$(this).parent().removeClass("has-success");
-			$(this).parent().addClass("has-error");
-		}
-		updateSubmitStatus();
-
-	});
         })
 
-
+	
 });
